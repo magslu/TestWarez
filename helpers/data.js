292391f2b69fa2dd2c3f8617eb376e3306f3f2a2
magslu <magdalena.slusiewicz@gmail.com>
2024-11-06 15:59:03 +0100

    CI TEST 5

--- helpers/data.js
+++ helpers/data.js
@@ -2,4 +2,4 @@ export const userID = "a62d9719-4f37-437c-a582-a5a7db364f18"
 export const baseUrl = "https://demoqa.com"
 export const user = "user_magda"
 export const password = process.env.SECRET_PASSWORD
-export const testowa_zmienna = "test"
+// export const testowa_zmienna = "test"
